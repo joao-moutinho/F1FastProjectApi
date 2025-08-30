4353a0ea39f7a5aa92b15e8a1fd88dc9aea56922
joao.moutinho@mindera.com <joao.moutinho@mindera.com>
2025-08-30 16:58:22 +0100

    fix main.py

--- main.py
+++ main.py
@@ -6,6 +6,13 @@ from fastf1_service import get_pilot_classification, get_team_classification
 
 app = FastAPI()
 
+import os
+import uvicorn
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+
 @app.get("/")
 def root():
     return {"message": "API FastF1 ativa"}
